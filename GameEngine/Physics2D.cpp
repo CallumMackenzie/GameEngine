ba8912da1df01192647048f1bee61dee60ca78c6
Callum Mackenzie <Alexxander1611@gmail.com>
2021-03-14 11:55:11 -0600

    Worked on circle-rectangle collisions.

--- GameEngine/Physics2D.cpp
+++ GameEngine/Physics2D.cpp
@@ -27,6 +27,7 @@ Physics2D::CollisionData Physics2D::colliding(Hitbox2D hb1, Hitbox2D hb2, Vector
 	hb1.position.add(hb1Speed);
 	hb2.position.add(hb2Speed);
 
+	// CIRCLE - CIRCLE COLLISION
 	if (hb1.type == Hitbox2D::TYPE_CIRCLE && hb2.type == Hitbox2D::TYPE_CIRCLE) {
 		Vector2 posDifference = Vector2::subtract(hb1.circleCentre(), hb2.circleCentre());
 		float combinedRadii = hb1.circleRadius() + hb2.circleRadius();
@@ -49,6 +50,7 @@ Physics2D::CollisionData Physics2D::colliding(Hitbox2D hb1, Hitbox2D hb2, Vector
 		}
 		return CollisionData(COLLISION_NONE, DEFAULT_COLLISION_DIST);
 	}
+	// RECTANGLE - RECTANGLE COLLISION
 	if (hb1.type == Hitbox2D::TYPE_RECTANGLE && hb2.type == Hitbox2D::TYPE_RECTANGLE) {
 		if (hb1.position.x <= hb2.position.x + hb2.rectSize().x &&
 			hb1.position.x + hb1.rectSize().x >= hb2.position.x &&
@@ -86,6 +88,7 @@ Physics2D::CollisionData Physics2D::colliding(Hitbox2D hb1, Hitbox2D hb2, Vector
 
 		return CollisionData(COLLISION_NONE, DEFAULT_COLLISION_DIST);
 	}
+	// RECTANGLE - CIRCLE COLLISION
 	if ((hb1.type == Hitbox2D::TYPE_CIRCLE && hb2.type == Hitbox2D::TYPE_RECTANGLE) || (hb1.type == Hitbox2D::TYPE_RECTANGLE && hb2.type == Hitbox2D::TYPE_CIRCLE)) {
 		bool swap = false;
 		if (hb2.type == Hitbox2D::TYPE_CIRCLE) {
@@ -115,18 +118,23 @@ Physics2D::CollisionData Physics2D::colliding(Hitbox2D hb1, Hitbox2D hb2, Vector
 		float distance = Vector2::hypotenuse(distanceVector);
 
 
-		if (distance <= hb1.circleRadius()) {
+		if (distance <= hb1.circleRadius()) { // If there is a collision
 
-			float L = hb2.rectPos().x;
-			float T = hb2.rectPos().y;
-			float R = hb2.rectPos().x + hb2.rectSize().x;
-			float B = hb2.rectPos().y + hb2.rectSize().y;
-			Vector2 hitVec = Vector2();
+			float L = hb2.rectPos().x; // Rectangle left side
+			float T = hb2.rectPos().y; // Rectangle top side
+			float R = hb2.rectPos().x + hb2.rectSize().x; // Rectangle right side
+			float B = hb2.rectPos().y + hb2.rectSize().y; // Rectangle bottom side
+
+			Vector2 hitVec = Vector2(); // The vector2 quantity repersenting the amount of overlap between the circle and rectangle to allow for easy collision resolution
+
+			// Hit direction - not used in calculations
 			int hitDir = hitDirection(hb1, hb2);
 			if (swap)
 				hitDir = hitDirection(hb2, hb1);
 
+			// Checking for side or corner collisions
 			if (hb1.circleCentre().x > L && hb1.circleCentre().x < R) {
+				// Side collision : top or bottom
 				if (hb1.circleCentre().y < T) {
 					// Top side
 					hitVec.y = (hb1.circleCentre().y + hb1.circleRadius()) - hb2.rectPos().y;
@@ -138,6 +146,7 @@ Physics2D::CollisionData Physics2D::colliding(Hitbox2D hb1, Hitbox2D hb2, Vector
 				return CollisionData(hitDir, hitVec);
 			}
 			else if (hb1.circleCentre().y > T && hb1.circleCentre().y < B) {
+				// Side collision : left or right
 				if (hb1.circleCentre().x < L) {
 					// Left side
 					hitVec.x = (hb1.circleCentre().x + hb1.circleRadius()) - hb2.rectPos().x;
@@ -148,35 +157,47 @@ Physics2D::CollisionData Physics2D::colliding(Hitbox2D hb1, Hitbox2D hb2, Vector
 				}
 				return CollisionData(hitDir, hitVec);
 			}
+			// TODO: find the proper values to populate the hit vector with on corner collisions
 			else if (hb1.circleCentre().x < L) {
+				// Corner collison : left corners
+
+				float r = hb1.circleRadius(); // Radius of circle
+				float cX = hb1.position.x; // X position of circle center
+				float cY = hb1.position.y; // Y position of circle center
+
 				if (hb1.circleCentre().y < T) {
-					// Top left corner
-					float a = hb2.rectUL().y - hb1.circleCentre().y;
-					float b = hb2.rectUL().x - hb1.circleCentre().x;
-					float c = Vector::qSqrt(a * a + b * b);
-					float theta = std::asin(b / c);
-					float epsilon = std::asin(a / c);
-					float r = hb1.circleRadius();
-					float z = r - c;
-					float x = r * std::sin(theta);
-					float y = r * std::sin(epsilon);
-					float p = x - a;
-					float q = Vector::qSqrt(z * z - p * p);
-					hitVec.y = p;
-					hitVec.x = q;
+					// Top left corner collision
+					float a = hb2.rectUL().y - hb1.circleCentre().y; // Y value of triangle formed between circle center and corner
+					float b = hb2.rectUL().x - hb1.circleCentre().x; // X value of triangle formed between circle center and corner
+					float c = Vector::qSqrt(a * a + b * b); // Hypoteunse of the triangle formed between circle center and corner
 				}
 				else {
-					// Bottom left corner
-
+					float a = hb2.rectBL().y - hb1.circleCentre().y; // Y value of triangle formed between circle center and corner
+					float b = hb2.rectBL().x - hb1.circleCentre().x; // X value of triangle formed between circle center and corner
+					float c = Vector::qSqrt(a * a + b * b); // Hypoteunse of the triangle formed between circle center and corner
+					// Bottom left corner collision
 				}
 				return CollisionData(hitDir, hitVec);
 			}
 			else if (hb1.circleCentre().x > R) {
+				// Corner collision : right corners
+
+				float r = hb1.circleRadius(); // Radius of circle
+				float cX = hb1.position.x; // X position of circle center
+				float cY = hb1.position.y; // Y position of circle center
+
 				if (hb1.circleCentre().y < T) {
-					// Top right corner
+					// Top right corner collision
+					float a = hb2.rectUR().y - hb1.circleCentre().y; // Y value of triangle formed between circle center and corner
+					float b = hb2.rectUR().x - hb1.circleCentre().x; // X value of triangle formed between circle center and corner
+					float c = Vector::qSqrt(a * a + b * b); // Hypoteunse of the triangle formed between circle center and corner
 				}
 				else {
-					// Bottom right corner
+					// Bottom right corner collision
+
+					float a = hb2.rectBR().y - hb1.circleCentre().y; // Y value of triangle formed between circle center and corner
+					float b = hb2.rectBR().x - hb1.circleCentre().x; // X value of triangle formed between circle center and corner
+					float c = Vector::qSqrt(a * a + b * b); // Hypoteunse of the triangle formed between circle center and corner
 				}
 				return CollisionData(hitDir, hitVec);
 			}
